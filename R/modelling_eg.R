6a058ca52b0de06249a0f7f5b6d0a5b4ddfe77b5
benjaminmisiuk <benjaminmisiuk@gmail.com>
2024-11-24 15:41:39 -0330

    add data and code

---
+++ R/modelling_eg.R
@@ -0,0 +1,43 @@
+#load the data
+df <- read.csv("D:/Documents/GitHub/GGSA_R_Workshop/data/canada_beer.csv")
+
+plot(df$temp, df$breweries_per_100k)
+plot(df$precip, df$breweries_per_100k)
+
+#fit a linear model between precipitation and breweries per 100k
+lm_mod <- lm(formula = breweries_per_100k ~ precip, data = df)
+#predict the x values on the plot using the model
+df$lm <- predict(lm_mod, df)
+i <- order(df$precip)
+lines(df$precip[i], df$lm[i])
+
+lm_mod
+
+#fit a random forest
+library(randomForest)
+rf_mod <- randomForest(formula = breweries_per_100k ~ precip, data = df)
+#predict the x values on the plot using the model
+df$rf <- predict(rf_mod, df)
+lines(df$precip[i], df$rf[i], col = 'red')
+
+#fit a MARS model
+library(earth)
+mars_mod <- earth(formula = breweries_per_100k ~ precip, data = df)
+#predict the x values on the plot using the model
+df$mars <- predict(mars_mod, df)
+lines(df$precip[i], df$mars[i], col = 'blue')
+
+#predict how many breweries there would be in a city with 1000mm of precipitation
+predict(lm_mod, data.frame(precip = 1000))
+predict(rf_mod, data.frame(precip = 1000))
+predict(mars_mod, data.frame(precip = 1000))
+
+#fit models with both temperature and precipitation
+lm_mod <- lm(formula = breweries_per_100k ~ temp + precip, data = df)
+rf_mod <- randomForest(formula = breweries_per_100k ~ temp + precip, data = df)
+mars_mod <- earth(formula = breweries_per_100k ~ temp + precip, data = df)
+
+#predict how many breweries there would be in a city with 1000mm of precipitation and 5 degrees
+predict(lm_mod, data.frame(temp = 5, precip = 1000))
+predict(rf_mod, data.frame(temp = 5, precip = 1000))
+predict(mars_mod, data.frame(temp = 5, precip = 1000))
